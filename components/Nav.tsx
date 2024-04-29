313644fe32eeeb9d5e4dd3a594caa8ea7cc96bc9
AnmolSaini16 <sainianmol16@gmail.com>
2024-04-29 14:15:03 +0530

    Added soruce code link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link";
+import { Github } from "lucide-react";
+
 import { Button } from "./ui/button";
 
 export default function Nav() {
   return (
     <nav className="py-4 border-b-2 border-gray-900/6 flex justify-between items-center">
       <h1 className="text-2xl font-medium">Next 14 Infinite Scroll</h1>
-      <Button variant="outline">Source Code</Button>
+      <Button variant="outline" asChild>
+        <Link
+          href="https://github.com/AnmolSaini16/next-14-infinite-scroll"
+          target="_blank"
+        >
+          <Github className="w-4 h-4 mr-2" />
+          Source code
+        </Link>
+      </Button>
     </nav>
   );
 }
